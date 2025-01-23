e366eaa4a531027e524989c2d7e01991f8a0d383
Maycon Kabitschke <mayconkabitschke@gmail.com>
2025-01-23 10:03:27 -0300

    data reativa

--- src/Context/DataContext.tsx
+++ src/Context/DataContext.tsx
@@ -6,6 +6,10 @@ type IDataContext = {
   data: IVenda[] | null;
   loading: boolean;
   error: string | null;
+  inicio: string;
+  final: string;
+  setInicio: React.Dispatch<React.SetStateAction<string>>;
+  setFinal: React.Dispatch<React.SetStateAction<string>>;
 }
 
 type IVenda = {
@@ -27,14 +31,25 @@ export const useData = () => {
   return context;
 };
 
+function getDate(n: number) {
+  const date = new Date();
+  date.setDate(date.getDate() - n);
+  const dd = String(date.getDate()).padStart(2, '0');
+  const mm = String(date.getMonth() + 1).padStart(2, '0');
+  const yyyy = date.getFullYear();
+  return `${yyyy}-${mm}-${dd}`;
+}
+
 
 //2° Criar o Provider
 export const DataContextProvider = ({ children }: React.PropsWithChildren) => {
-  const { data, loading, error } = useFetch<IVenda[]>('https://data.origamid.dev/vendas/',
+  const [inicio, setInicio] = React.useState(getDate(14));
+  const [final, setFinal] = React.useState(getDate(0));
+  const { data, loading, error } = useFetch<IVenda[]>(`https://data.origamid.dev/vendas/?inicio=${inicio}&final=${final}`,
   );
 
 
   //3° DataCOntext nome do contexto . Provider
-  return <DataContext.Provider value={{ data, loading, error }}>{children}</DataContext.Provider>
+  return <DataContext.Provider value={{ data, loading, error, inicio, setInicio, final, setFinal }}>{children}</DataContext.Provider>
 
 };
