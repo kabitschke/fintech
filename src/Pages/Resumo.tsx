6871365e78de251f589ea70a9fb4e454c6c87577
Maycon Kabitschke <mayconkabitschke@gmail.com>
2025-01-23 18:08:56 -0300

    resumo vendas

--- src/Pages/Resumo.tsx
+++ src/Pages/Resumo.tsx
@@ -4,11 +4,40 @@ const Resumo = () => {
 
   const { data } = useData();
 
-  return (
-    <div>
-      <h1>Resumo</h1>
+  if (data === null) return null;
 
-    </div>
+  return (
+    <section>
+      <div className="resumo flex mb">
+        <div className="box">
+          <h2>Vendas</h2>
+          <span>
+            {
+              data.filter((i) => i.status !== 'falha').reduce((acc, item) => acc + item.preco, 0).toLocaleString("pt-br", { style: 'currency', currency: 'BRL' })
+            }
+          </span>
+        </div>
+
+        <div className="box">
+          <h2>Recebido</h2>
+          <span>
+            {
+              data.filter((i) => i.status === 'pago').reduce((acc, item) => acc + item.preco, 0).toLocaleString("pt-br", { style: 'currency', currency: 'BRL' })
+            }
+          </span>
+        </div>
+
+        <div className="box">
+          <h2>Processando</h2>
+          <span>
+            {
+              data.filter((i) => i.status === 'processando').reduce((acc, item) => acc + item.preco, 0).toLocaleString("pt-br", { style: 'currency', currency: 'BRL' })
+            }
+          </span>
+        </div>
+      </div>
+      <div className="box">Gráficos</div>
+    </section>
   );
 }
 
